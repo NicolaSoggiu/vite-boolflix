ee3ff7de677e3b055e2c0a4b04f92e0bba70f825
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-24 15:37:13 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppContainer from './components/AppContainer.vue';
+import axios from 'axios';
 import { store } from "./store";
+
 
 export default {
     data() {
         return {
             store,
-        }
+        };
+    },
+    components: {
+        AppHeader, AppContainer,
+    },
+    methods: {
+
     }
 }
 </script>
 
 <template>
+
+  <AppHeader/>
+  <AppContainer/>
   
 </template>
 
---
+++ src/components/AppContainer.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from "../store"
+import AppMovieSeries from "./AppMovieSeries.vue";
+
+export default {
+    data() {
+        return {
+            store,
+        };
+    },
+    components: {
+        AppMovieSeries,
+    },
+}
+</script>
+
+<template>
+
+    <!-- MOVIES -->
+    <div>
+        <AppMovieSeries/>
+    </div>
+    
+    <!-- SERIES -->
+    <div>
+        <AppMovieSeries/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
