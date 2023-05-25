bf43faefaa8e85988ec4d56d44a8bbc9fd3e895e
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-25 17:30:43 +0200

    start milestone 4 (added an initial style)

--- src/components/AppMovieSeries.vue
+++ src/components/AppMovieSeries.vue
@@ -23,39 +23,80 @@ export default {
 </script>
 
 <template>
-  <div class="card">
-    <ul>
-      <li>{{ title }}</li>
-      <li>{{ originalTitle }}</li>
-      <li>{{ language }}</li>
-      <span>VOTO: </span>
-      <font-awesome-icon
-        class="star"
-        v-for="star in rating(vote)"
-        :key="star"
-        :icon="['fas', 'star']"
+  <div class="container-card">
+    <div class="card">
+      <img
+        class="poster"
+        :src="`http://image.tmdb.org/t/p/w342${image}`"
+        :alt="image"
       />
-      <template v-for="star in 5 - rating(vote)">
+      <div class="info">
+        <div class="description">
+          <span class="details">{{ title }}</span>
+        </div>
+        <div class="description">
+          <span class="details">{{ originalTitle }}</span>
+        </div>
+        <div class="description">
+          <span class="details">{{ language }}</span>
+        </div>
+        <span>VOTO: </span>
         <font-awesome-icon
           class="star"
+          v-for="star in rating(vote)"
           :key="star"
-          :icon="['far', 'star']"
-          v-if="rating(vote) < 5"
+          :icon="['fas', 'star']"
         />
-      </template>
-      <img :src="`http://image.tmdb.org/t/p/w342${image}`" :alt="image" />
-    </ul>
+        <template v-for="star in 5 - rating(vote)">
+          <font-awesome-icon
+            class="star"
+            :key="star"
+            :icon="['far', 'star']"
+            v-if="rating(vote) < 5"
+          />
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-ul {
-  padding: 2rem;
-  .image {
-    width: 500px;
+.container-card {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 30px;
+  width: 100%;
+  .poster {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
-  .star {
-    color: yellow;
+}
+
+.star {
+  color: yellow;
+}
+
+.card {
+  margin: 50px auto;
+  height: 426px;
+  font-weight: bold;
+  background-color: black;
+  box-shadow: 5px 5px 14px 3px black;
+  .info {
+    display: none;
   }
+}
+
+.card:hover {
+  padding: 10px;
+  box-shadow: none;
+}
+.card:hover .poster {
+  display: none;
+}
+.card:hover .info {
+  display: block;
+  color: white;
 }
 </style>
